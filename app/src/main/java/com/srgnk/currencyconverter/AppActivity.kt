24631e07bccd77aab02eb3793e370f4c55f8d17e
E-Srgnk <e.srgnk@gmail.com>
2021-07-21 23:41:50 +0300

    removed unnecessary code and added hilt annotation

--- app/src/main/java/com/srgnk/currencyconverter/AppActivity.kt
+++ app/src/main/java/com/srgnk/currencyconverter/AppActivity.kt
@@ -2,15 +2,12 @@ package com.srgnk.currencyconverter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.srgnk.currencyconverter.databinding.ActivityAppBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AppActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityAppBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +15,5 @@ class AppActivity : AppCompatActivity() {
 
         binding = ActivityAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
